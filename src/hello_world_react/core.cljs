e062668ed34bc04ce0098c834d212ef5c96c8654
Rafael Herrera <santoElias65537@gmail.com>
2022-02-12 23:35:23 -0700

    render not working, Y?

--- src/hello_world_react/core.cljs
+++ src/hello_world_react/core.cljs
@@ -1,62 +1,21 @@
 (ns hello-world-react.core
-  (:require [reagent.core :as r]
-            [clojure.pprint]))
+  (:require [reagent.core :as r]))
 
-;; NOTE defonce is required so that hot reloading w/ figwheel doesn't reset the state
-(defonce todos (r/atom []))
+(+ 4 4 (+ 9 9))
 
-(defn add-todo [text]
-  (swap! todos conj {:text text :done? false}))
-
-(defn vec-remove
-  "remove elem in coll"
-  [coll pos]
-  (vec (concat (subvec coll 0 pos)
-               (subvec coll (inc pos)))))
-
-;; NOTE ditto for the state initialization process
-(defonce init
-  (do (add-todo "I like trains")
-      (add-todo "buy milk")))
-
-;; -------------------------
-;; Views
-
-(def ^:const +return-key+ 13)
-
-(defn todo-item [id {:keys [text done?]}]
-  (let [html-id  (str "todo" id)]
-    [:tr
-     [:td>input {:type :checkbox
-                 :checked done?
-                 :id html-id
-                 :on-change #(swap! todos update-in [id :done?] not)}]
-     [:td>label {:for html-id} text]
-     [:td>a {:href "#"
-             :on-click #(swap! todos vec-remove id)
-             :style {:margin-left 7}}
-      "🗑"]]))
-
-(defn form-todo []
-  [:div.form-todo
-   [:label {:for 'text-input} "Add a todo: "]
-   [:input#text-input {:on-key-down #(when (= (.-which %) +return-key+)
-                                       (let [ti (.getElementById js/document "text-input")]
-                                         (add-todo (.-value ti))
-                                         (set! (.-value ti) "")))}]])
-
-(defn home-page []
+(defn my-div []
   [:div
-   [:h1 "TODO"]
-   [:table>tbody
-    (map todo-item (iterate inc 0) @todos)]
-   (form-todo)
-   [:h2 "Raw state content"]
-   [:pre (with-out-str
-           (clojure.pprint/pprint @todos))]])
+   [:p "fucking works!"]])
+
+(defn my-home-page []
+  [:div (my-div)])
+
+
+(+ 4 2354)
 
 ;; -------------------------
 ;; Initialize app
 
 (defn init! []
-  (r/render [home-page] (.getElementById js/document "app")))
+  (r/render [my-home-page] (.getElementById js/document "app")))
+
